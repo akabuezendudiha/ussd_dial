dc94c5ba059068250f9f94751134140dadc745e2
akabuezendudiha <akabuezendudiha@gmail.com>
2022-08-27 20:50:01 +0100

    Updated the Cell Number page title to Settings: Cell Number

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData( 
         primarySwatch: Colors.blue,
       ), 
-      home: CellNumberPage(title: "Save Cell Number"),
+      home: CellNumberPage(title: "Settings: Cell Number"),
     );
   }
 }
